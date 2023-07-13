a29ae7d2b948cd74437e27fc28977079e3e29669
selva-fsd <125110925+selva-fsd@users.noreply.github.com>
2023-07-13 19:13:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function get() {
+
+    var first=document.getElementById("fn").value;
+    var middle=document.getElementById("mn").value;
+    var last=document.getElementById("ln").value;
+    var em=document.getElementById("mail").value;
+    console.log(first);
+    console.log(middle);
+    console.log(last);
+    console.log(em);
+
+}
